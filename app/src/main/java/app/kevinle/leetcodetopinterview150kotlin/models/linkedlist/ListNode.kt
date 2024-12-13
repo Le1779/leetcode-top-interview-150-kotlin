671ead186a3b349015cd4ab940c5df136c2b76e5
kevinle <le8419951779@gmail.com>
2024-12-13 15:06:44 +0800

    feat: add solution for problem 2593 - Find Score of an Array After Marking All Elements

---
+++ app/src/main/java/app/kevinle/leetcodetopinterview150kotlin/models/linkedlist/ListNode.kt
@@ -0,0 +1,8 @@
+package app.kevinle.leetcodetopinterview150kotlin.models.linkedlist
+
+/**
+ * Created by Kevin Le on 2024/11/4.
+ */
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
---
+++ app/src/main/java/app/kevinle/leetcodetopinterview150kotlin/p148/148. Sort List.kt
@@ -0,0 +1,12 @@
+package app.kevinle.leetcodetopinterview150kotlin.p148
+
+import app.kevinle.leetcodetopinterview150kotlin.models.linkedlist.ListNode
+
+/**
+ * Created by Kevin Le on 2024/11/4.
+ */
+class Solution {
+    fun sortList(head: ListNode?): ListNode? {
+
+    }
+}
